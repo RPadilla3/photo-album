e955ee5742d3337476fea4b4e3e9853c3d60d235
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-10-31 14:17:04 -0400

    appended the nav element to the li's when they are clicked individually.

--- javascript/photoAlbum.js
+++ javascript/photoAlbum.js
@@ -4,15 +4,12 @@
     console.log($('main'));
 
     $('.photos')
-
         .find('li')
           .on('click', function zoomIng(eventObj) {
             $(this).toggleClass('zoomed')
-        .find('main')
-          .on('click', function zoomIn(eventObj) {
-          $(this).toggleClass('zooming')
+            $(this).closest('main').toggleClass('zooming')
+            $(this).append( $('nav') );
 
-          })
     });
 
 })();
